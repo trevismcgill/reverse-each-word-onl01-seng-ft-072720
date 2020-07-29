ccd3adf8108132bd3a34edf9bbc50a1b5234b465
Trevis McGill <trevismcgill25@gmail.com>
2020-07-29 18:45:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,5 @@ def reverse_each_word(string)
   reversed_phrase = []
   split_phrase = string.split
   split_phrase.collect do |word|
-    reversed_phrase.push(word.reverse)
   end
-  reversed_phrase.join(" ")
 end
